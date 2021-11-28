db9e70b4152ab3e21318f030f150d1638b5d0eec
Paridhi Jain <paridhijain0201@gmail.com>
2021-11-28 12:33:47 +0530

    Cart counter added

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const path = require("path");
 const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
-const session = require("express-session");
+const session = require("express-session")
 const flash = require("express-flash");
 const MongoDbStore = require("connect-mongo");
 app.use(express.json());
+
 // const bodyParser = require('body-parser')
 //Database connection
-mongoose.connect("mongodb://localhost:27017/pizza", { useNewUrlParser: true });
-
+mongoose.connect("mongodb://localhost:27017/pizzaDB", { useNewUrlParser: true });
 
 //Session configuration
 app.use(
@@ -23,15 +23,22 @@ app.use(
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24 },
      store: MongoDbStore.create({
-         mongoUrl:'mongodb://localhost:27017/pizza'
+         mongoUrl:'mongodb://localhost:27017/pizzaDB'
      }) // 24 hour
   })
 );
+app.use((req,res,next)=>{
+  res.locals.session=req.session
+  next()
+})
+
 app.use(flash());
 require("./routes/web.js")(app);
 //Assets
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
+
 // app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(express.urlencoded({ extended: false }))
