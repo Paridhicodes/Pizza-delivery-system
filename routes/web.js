7e7fbc0f22373dac3049844213051ba0ecae017d
Paridhi Jain <paridhijain0201@gmail.com>
2021-12-01 18:39:18 +0530

    order tracker

--- routes/web.js
+++ routes/web.js
@@ -3,35 +3,31 @@ const authController=require('../app/http/controllers/authController');
 const cartController=require('../app/http/controllers/customers/cartController');
 const orderController=require('../app/http/controllers/customers/orderController');
 const menuController=require('../app/http/controllers/menuController');
+const AdminOrderController=require('../app/http/controllers/admin/orderController');
+
+//Middlewares
 const guest=require('../app/http/middlewares/guest')
-function initRoutes(app){
-    
-    app.get('/',homeController().index)
-//     (req,res)=>{
-//     res.render('home');
-// }
+const auth=require('../app/http/middlewares/auth')
+const admin=require('../app/http/middlewares/admin')
+
+function initRoutes(app){    
+app.get('/',homeController().index)
 app.get('/login',guest,authController().login)
 app.post('/login',authController().postLogin)
 app.get('/register',guest,authController().register)
 app.post('/register',authController().postRegister)
-// (req,res)=>{
-//     res.render('login');
-// }
-// app.post('/login',authController().postLogin)
-// app.get('/register',authController().register)
-// app.post('/register',authController().postRegister)
+
 app.post('/logout',authController().logout)
 app.get('/menu',menuController().index)
-// (req,res)=>{
-//     res.render('menu');
-// }
+
 app.get('/cart',cartController().index)
-// (req,res)=>{
-//     res.render('customers/cart');
-// }
+
 app.post('/update-cart',cartController().update)
-app.post('/orders',orderController().store)
-app.get('/customers/orders',orderController().index)
+app.post('/orders',auth,orderController().store)
+app.get('/customers/orders',auth,orderController().index)
+
+//Admin routes
+app.get('/admin/orders',admin,AdminOrderController().index);
 }
 
 
